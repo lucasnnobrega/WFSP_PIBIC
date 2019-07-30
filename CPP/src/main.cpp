f515dde5efc3af303b85238dc400540e214381bb
lucasnnobrega <lucasnnobrega@gmail.com>
2019-07-30 19:15:39 -0300

    Removed index files

--- CPP/src/main.cpp
+++ CPP/src/main.cpp
@@ -6,7 +6,6 @@
 // comentario normal
 //* comentario mais claro
 
-
 #include <ilcplex/ilocplex.h>
 #include <iostream>
 #include <math.h>
@@ -18,13 +17,13 @@
 
 using namespace std;
 
-// Driver function to sort the 2D vector 
-// on basis of a particular column 
-bool sortByCol( const vector<int>& v1, 
-              const vector<int>& v2) { 
- return v1[1] < v2[1]; 
-} 
-
+// Driver function to sort the 2D vector
+// on basis of a particular column
+bool sortByH(const vector<int> &v1,
+             const vector<int> &v2)
+{
+    return v1[2] < v2[2];
+}
 
 void WFSP(int number_of_symbols, int m, int *priorities)
 {
@@ -35,7 +34,7 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     //* Dados
 
     // Numero de Simbolos
-    std::cout << "Inside WFSP function"  << std::endl;
+    std::cout << "Inside WFSP function" << std::endl;
     int n = number_of_symbols;
     std::cout << "Number of symbols: " << number_of_symbols << " " << n << std::endl;
 
@@ -45,32 +44,27 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     // Numero Maximo de copias do simbolo Xi E X
     int M[n];
 
-     for(int i = 0; i < n; i++){
-         M[i] = m;
+    for (int i = 0; i < n; i++)
+    {
+        M[i] = m;
     }
 
-
-    
-
     // Conjunto de indices das cópias xi E X,  Ki = {1, ..., Mi}
-    // i  = {1,...N} 
+    // i  = {1,...N}
     // Array de ponteiros
     int *K[n];
-    
 
-    for (int i = 0; i < n; i++) {
-        
+    for (int i = 0; i < n; i++)
+    {
 
         int *aux = new int(M[i]);
         K[i] = aux;
 
-        for (int j = 0; j < M[i]; j++) {
+        for (int j = 0; j < M[i]; j++)
+        {
             K[i][j] = j;
         }
-
-        
     }
-    
 
     // Tamanho maximo das sequencias viaveis
     int TMAX = m * n;
@@ -79,108 +73,116 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     int c[n];
 
     std::cout << std::endl;
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         c[i] = priorities[i];
         std::cout << "Priority: " << c[i] << " ";
     }
-     std::cout << std::endl;
+    std::cout << std::endl;
 
-    
     // Conjunto de posicoes que podem ser ocupadas pela k-ésima cópia do simbolo Xi E X
     // Hik = {k, ... ,TMAX}
     int **H[n];
     std::cout << "Creating the array Hik:" << std::endl;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         //H[i] = new int*[M[i]];
-        
-        H[i] = new int*[M[i]];
-        
-        for (int k = 0; k < M[i]; k++) {
-            int aux = 0;          
-          
+
+        H[i] = new int *[M[i]];
+
+        for (int k = 0; k < M[i]; k++)
+        {
+            int aux = 0;
+
             H[i][k] = new int[TMAX - k];
- 
-            
-            for (int j = k; j < TMAX; j++) {
+
+            for (int j = k; j < TMAX; j++)
+            {
                 H[i][k][aux] = j;
-                aux++;              
+                aux++;
             }
         }
     }
-    
+
     //* Verify input data
     std::cout << "#####################################" << std::endl;
     std::cout << "Verify input data: " << std::endl;
     std::cout << "Number of symbols: " << n << std::endl;
     std::cout << "TMAX: " << TMAX << std::endl;
     std::cout << "#####################################" << std::endl;
-          
 
-    std::cout<<"Dados Criados, Agora, gerar as variáveis: "<<std::endl;
-    std::cout<<"Created data, now creating the variables: "<<std::endl;
+    std::cout << "Dados Criados, Agora, gerar as variáveis: " << std::endl;
+    std::cout << "Created data, now creating the variables: " << std::endl;
 
     //* Variaveis
-    
+
     // y_ikh , variavel binaria
     // Dimensão n pois i vai até n
-    IloArray <IloArray <IloBoolVarArray> > y(env, n);
+    IloArray<IloArray<IloBoolVarArray>> y(env, n);
 
-    for (int i = 0; i < n; i++) {
-        IloArray<IloBoolVarArray> vetorAux(env, M[i]); 
+    for (int i = 0; i < n; i++)
+    {
+        IloArray<IloBoolVarArray> vetorAux(env, M[i]);
         y[i] = vetorAux;
 
-        for (int k = 0; k < M[i]; k++) {
+        for (int k = 0; k < M[i]; k++)
+        {
             IloBoolVarArray vetorAux2(env, TMAX - k);
             y[i][k] = vetorAux2;
         }
     }
 
-
-    // Adicionando variavel no modelo    
-    for (int i = 0; i < n; i++) {        
-        for (int k = 0; k < M[i]; k++) {        
-            for(int h = 0; h < TMAX - k; h++) {
+    // Adicionando variavel no modelo
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i]; k++)
+        {
+            for (int h = 0; h < TMAX - k; h++)
+            {
                 char var[100];
                 sprintf(var, "y(%d,%d,%d)", i, k, H[i][k][h]);
                 y[i][k][h].setName(var);
                 modelo.add(y[i][k][h]);
-            } 
+            }
         }
     }
 
-
-
     // d_i,k,k+1
     // Dimensao n, pois i vai ate n
-    IloArray < IloIntVarArray > d(env, n);
-    for(int i = 0; i < n; i++) {
-        IloIntVarArray vetorAux(env, M[i], 0, IloInfinity); 
+    IloArray<IloIntVarArray> d(env, n);
+    for (int i = 0; i < n; i++)
+    {
+        IloIntVarArray vetorAux(env, M[i], 0, IloInfinity);
         d[i] = vetorAux;
     }
 
-    // Adicionando variavel no modelo 
-    for(int i = 0; i < n; i++) {        
-        for(int k = 0; k < M[i]; k++) {
+    // Adicionando variavel no modelo
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i]; k++)
+        {
             char var[100];
-            if(k == M[i] - 1 ) {
+            if (k == M[i] - 1)
+            {
                 sprintf(var, "d(%d, %d, %d)", i, k, 0);
             }
-            else {
-                sprintf(var, "d(%d, %d, %d)", i-1, k, k);
+            else
+            {
+                sprintf(var, "d(%d, %d, %d)", i - 1, k, k);
             }
             d[i][k].setName(var);
             modelo.add(d[i][k]);
-        } 
+        }
     }
-
 
     // D_i
     // Maior Distancia entre duas copias consecutivas do simbolo x_i E X
     IloIntVarArray D(env, n, 0, IloInfinity);
 
-    // Adicionando variavel no modelo    
-    for(int i = 0; i < n; i++){
+    // Adicionando variavel no modelo
+    for (int i = 0; i < n; i++)
+    {
         char var[100];
         sprintf(var, "D(%d)", i);
         D[i].setName(var);
@@ -192,7 +194,7 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     //? Implementado como um valor unico
     IloIntVar P(env, 0, IloInfinity);
 
-    // Adicionando variavel no modelo    
+    // Adicionando variavel no modelo
     char var[100];
     sprintf(var, "P");
     P.setName(var);
@@ -200,128 +202,149 @@ void WFSP(int number_of_symbols, int m, int *priorities)
 
     // p_ik
     // Posicao da k-esima copia do simbolo x_i E X
-    IloArray <IloIntVarArray>  p(env, n);
-    for (int i = 0; i < n; i++) {
-        IloIntVarArray vetorAux(env, M[i], 0, IloInfinity); 
+    IloArray<IloIntVarArray> p(env, n);
+    for (int i = 0; i < n; i++)
+    {
+        IloIntVarArray vetorAux(env, M[i], 0, IloInfinity);
         p[i] = vetorAux;
     }
 
-    // Adicionando variavel no modelo 
-    for(int i = 0; i < n; i++) {        
-        for(int k = 0; k < M[i]; k++) {
+    // Adicionando variavel no modelo
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i]; k++)
+        {
             char var[100];
             sprintf(var, "p(%d,%d)", i, k);
             p[i][k].setName(var);
             modelo.add(p[i][k]);
-        } 
+        }
     }
- 
 
-    // Criando a Função Objetivo (FO) 
+    // Criando a Função Objetivo (FO)
     // min P
     IloObjective obj(env, P, IloObjective::Minimize);
     modelo.add(obj);
- 
-    // Constraints 
+
+    // Constraints
 
     // P >= D_i * c_i, para i = 1, ... , n (4.2) (3)
-    for(int i = 0; i < n; i++){
-        modelo.add( P >= D[i]*c[i] );
+    for (int i = 0; i < n; i++)
+    {
+        modelo.add(P >= D[i] * c[i]);
     }
 
     // D_i ≥ d_i,k,k+1 , para todo i = 1, . . . , n,  para todo k ∈ K_i \ {M_i }, (4.3) (4)
-    for(int i = 0; i < n; i++){
-        for(int k = 0; k < M[i]-1; k++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i] - 1; k++)
+        {
             modelo.add(D[i] >= d[i][k]);
         }
     }
 
-    // sum{i in X, k in K[i]} y_ikh ≤ 1, ∀h ∈ {1, . . . , T MAX}, (4.4) (5)    
-    for(int h = 0; h < TMAX-1; h++){
+    // sum{i in X, k in K[i]} y_ikh ≤ 1, ∀h ∈ {1, . . . , T MAX}, (4.4) (5)
+    for (int h = 0; h < TMAX - 1; h++)
+    {
         IloExpr soma(env);
-        for (int i = 0; i < n; i++) {  
-            for (int k = 0; k < M[i]; k++) { 
-                if (h < TMAX - k)     
-                    soma += y[i][k][h];            
+        for (int i = 0; i < n; i++)
+        {
+            for (int k = 0; k < M[i]; k++)
+            {
+                if (h < TMAX - k)
+                    soma += y[i][k][h];
             }
         }
         modelo.add(soma <= 1);
     }
 
-    // sum{h in H[i][k]} y_ikh ≤ 1,, (4.4) (6)     
-    for (int i = 0; i < n; i++) {  
-        for (int k = 0; k < M[i]; k++) {      
+    // sum{h in H[i][k]} y_ikh ≤ 1,, (4.4) (6)
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i]; k++)
+        {
             IloExpr soma(env);
-            for (int h = 0; h < TMAX - k; h++){
-                soma += y[i][k][h];            
+            for (int h = 0; h < TMAX - k; h++)
+            {
+                soma += y[i][k][h];
             }
             modelo.add(soma <= 1);
         }
     }
 
-    // sum{k in K[i], h in H[i][k]} y_ikh ≤ 1,  (4.4) (7)    
-    for (int i = 0; i < n; i++) {  
+    // sum{k in K[i], h in H[i][k]} y_ikh ≤ 1,  (4.4) (7)
+    for (int i = 0; i < n; i++)
+    {
         IloExpr soma(env);
-        for (int k = 0; k < M[i]; k++) {      
-            for(int h = 0; h < TMAX - k; h++){
-                soma += y[i][k][h];            
+        for (int k = 0; k < M[i]; k++)
+        {
+            for (int h = 0; h < TMAX - k; h++)
+            {
+                soma += y[i][k][h];
             }
         }
         modelo.add(soma <= 1);
     }
-    
+
     // (8)
-    for(int i = 0; i < n; i++) {
-        for (int k = 0; k < M[i]; k++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i]; k++)
+        {
             IloExpr soma(env);
-            for (int h = 0; h < TMAX - k; h++) {
-                soma += y[i][k][h]*H[i][k][h];
+            for (int h = 0; h < TMAX - k; h++)
+            {
+                soma += y[i][k][h] * H[i][k][h];
             }
             modelo.add(p[i][k] == soma);
         }
     }
 
     // (9)
-    for(int i = 0; i < n; i++) {
-        for (int k = 0; k < M[i] - 1; k++){
-            
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i] - 1; k++)
+        {
 
             IloExpr soma_esq(env);
             IloExpr soma_dir(env);
             //?for (int h = 0; h < TMAX - k ; h++)
-            for (int h = 0; h < TMAX - k - 1; h++) {
-            
-                soma_esq += y[i][k][h];
-                soma_dir += y[i][k+1][h];
+            for (int h = 0; h < TMAX - k - 1; h++)
+            {
 
+                soma_esq += y[i][k][h];
+                soma_dir += y[i][k + 1][h];
             }
             modelo.add(soma_esq >= soma_dir);
         }
     }
 
-
-
     // (10) Tese (4.9)
-    for(int i = 0; i < n; i++) {
-        for (int k = 0; k < M[i] - 1; k++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i] - 1; k++)
+        {
             IloExpr soma(env);
             //?for (int h = 0; h < TMAX - k -1; h++) {
-            for (int h = 0; h < TMAX - k -1; h++) {
-                soma += y[i][k+1][h];
+            for (int h = 0; h < TMAX - k - 1; h++)
+            {
+                soma += y[i][k + 1][h];
             }
-            modelo.add(p[i][k+1] >= p[i][k] - (1 - soma)*TMAX);
+            modelo.add(p[i][k + 1] >= p[i][k] - (1 - soma) * TMAX);
         }
     }
 
     // (11) Tese (4.10)
-    for(int i = 0; i < n; i++) {
-        for (int k = 0; k < M[i] - 1; k++){
-            for (int h = 0; h < TMAX - k; h++) {
-                modelo.add(d[i][k] >= p[i][k+1] - p[i][k]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int k = 0; k < M[i] - 1; k++)
+        {
+            for (int h = 0; h < TMAX - k; h++)
+            {
+                modelo.add(d[i][k] >= p[i][k + 1] - p[i][k]);
             }
         }
     }
-
 
     /* 
     // y_ikh E {0, 1}, para todo i = 1, . . . , n, para todo k ∈ K_i , para todo h ∈ H_ik. (4.13)
@@ -335,16 +358,15 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     }
     */
 
-
     // Creating a CPLEX solver
     IloCplex cplex(env);
     cplex.setParam(IloCplex::Param::Threads, 1);
     cplex.extract(modelo);
-    
+
     cplex.out() << endl;
     cplex.solve();
-    cplex.out() << endl << "solution status = " << cplex.getStatus() << endl;
-
+    cplex.out() << endl
+                << "solution status = " << cplex.getStatus() << endl;
 
     //cplex.out() << y << endl;
 
@@ -365,29 +387,30 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     }
     */
 
+    vector<vector<int>> sequence;
 
-
-   vector< vector< int > > sequence;
-
-    for (int i = 0; i < n; i++) {   // 2 == n
-        for (int k = 0; k < M[i]; k++) {
-            for (int h = 0; h < TMAX - k; h++){
+    for (int i = 0; i < n; i++)
+    { // 2 == n
+        for (int k = 0; k < M[i]; k++)
+        {
+            for (int h = 0; h < TMAX - k; h++)
+            {
                 //cplex.out() << " OUT = " << cplex.getValue(y[i][k][h]) << endl;
                 // If the value is equal to 1, save in the sequence vector
-                if ( cplex.getValue(y[i][k][h])  == 1 ){
-                    vector < int > aux;
+                if (cplex.getValue(y[i][k][h]) == 1)
+                {
+                    vector<int> aux;
                     aux.push_back(i);
                     aux.push_back(k);
-                    aux.push_back(h); 
+                    aux.push_back(h);
 
                     sequence.push_back(aux);
-                }                
+                }
             }
         }
     }
 
-
-    sort(sequence.begin(), sequence.end(), sortByCol);
+    sort(sequence.begin(), sequence.end(), sortByH);
 
     /*
     for (int i = 0; i < sequence.size(); i++) { 
@@ -402,39 +425,36 @@ void WFSP(int number_of_symbols, int m, int *priorities)
     } 
     */
 
-        Write_content content;
+    Write_content content;
 
-        content.objective = cplex.getObjValue();
-        
-        content.number_of_symbols = number_of_symbols; // OK
-        
+    content.objective = cplex.getObjValue();
 
-        int aux = 0;
-        for(auto i = sequence.begin(); i < sequence.end(); i++){
-            aux ++;
-        }
+    content.number_of_symbols = number_of_symbols; // OK
 
-        content.occupied_positions = aux;
+    int aux = 0;
+    for (auto i = sequence.begin(); i < sequence.end(); i++)
+    {
+        aux++;
+    }
 
+    content.occupied_positions = aux;
 
-        content.total_positions = TMAX; // OK
-        
-        content.avaliable_copies = priorities;
-        
-        content.used_copies = priorities;
-        
-        content.priorities = priorities;
+    content.total_positions = TMAX; // OK
 
-        int *nothing = NULL;
+    content.avaliable_copies = priorities;
 
-        content.Di = nothing;
-        content.Df = nothing;
-        content.Pi = nothing;
-        content.Pf = nothing;
-            
-        write_res('v', content); 
+    content.used_copies = priorities;
 
+    content.priorities = priorities; // OK
 
+    int *nothing = NULL;
+
+    content.Di = nothing;
+    content.Df = nothing;
+    content.Pi = nothing;
+    content.Pf = nothing;
+
+    write_res('v', content);
 
     cplex.out() << "\e[32mcost\e[0m = " << cplex.getObjValue() << endl;
 
@@ -446,18 +466,18 @@ int main(int argc, char *argv[])
     std::cout << "Inside the main function" << std::endl;
     try
     {
-        std::cout<<"Inside the try"<<std::endl;
+        std::cout << "Inside the try" << std::endl;
 
-        
         File_content *aux = NULL;
         aux = read_instances("./data/csp_instances/ins_05_20_4.txt");
 
-        if (aux){
-            std::cout<<"Inside the try -> IF"<<std::endl;
+        if (aux)
+        {
+            std::cout << "Inside the try -> IF" << std::endl;
 
             std::cout << "number of symbols: " << aux->number_of_symbols << std::endl;
-            std::cout << "number m: " << aux->m  << std::endl;
-            WFSP(aux->number_of_symbols, aux->m, aux->priorities); 
+            std::cout << "number m: " << aux->m << std::endl;
+            WFSP(aux->number_of_symbols, aux->m, aux->priorities);
             free(aux);
         }
     }
--- CPP/src/read_write.cpp
+++ CPP/src/read_write.cpp
@@ -4,51 +4,54 @@
 
 using namespace std;
 
-typedef struct Files_Content {
+typedef struct Files_Content
+{
   int number_of_symbols;
   int m;
   int *priorities;
 } File_content;
 
-typedef struct Write_Content {
+typedef struct Write_Content
+{
   int objective;
   int number_of_symbols;
   int occupied_positions;
   int total_positions;
-  int* avaliable_copies;
-  int* used_copies;
-  int* priorities;
-  int* Di;
-  int* Df;
-  int* Pi;
-  int* Pf;
+  int *avaliable_copies;
+  int *used_copies;
+  int *priorities;
+  int *Di;
+  int *Df;
+  int *Pi;
+  int *Pf;
 } Write_content;
 
-
-int verbose(){
-    return 0;
+int verbose()
+{
+  return 0;
 }
 
-int write(char verbose) {
-    if(verbose == 'v')
-      std::cout << "Criado o arquivo" << std::endl; 
-    
-    ofstream myfile;
-    myfile.open ("./data/example.txt");
-    myfile << "Writing this to a file.\n";
-    myfile.close();
-    return 0;
+int write(char verbose)
+{
+  if (verbose == 'v')
+    std::cout << "Criado o arquivo" << std::endl;
+
+  ofstream myfile;
+  myfile.open("./data/example.txt");
+  myfile << "Writing this to a file.\n";
+  myfile.close();
+  return 0;
 }
 
-File_content *read_instances(const char* relative_file_path) {
+File_content *read_instances(const char *relative_file_path)
+{
   string linhas[5];
-  
 
   File_content *content;
-  content = (File_content*)malloc(sizeof(File_content));
+  content = (File_content *)malloc(sizeof(File_content));
 
   //TODO: colocar o nome das instancias dinamico
-  ifstream myfile (relative_file_path);
+  ifstream myfile(relative_file_path);
   if (myfile.is_open())
   {
     int count = 4;
@@ -57,52 +60,57 @@ File_content *read_instances(const char* relative_file_path) {
       getline(myfile, linhas[i]);
       cout << linhas[i] << '\n';
     }
-  
-    cout << endl << endl;    
+
+    cout << endl
+         << endl;
 
     int number_of_symbols = linhas[0][0] - '0';
     int m = linhas[1][0] - '0';
 
-    int* myarray = new int[sizeof(linhas[2])]; 
-    int* myarray_clean = new int[sizeof(linhas[2])]; 
+    int *myarray = new int[sizeof(linhas[2])];
+    int *myarray_clean = new int[sizeof(linhas[2])];
     string to_compare = " ";
 
-    for (int i = 0; i < linhas[2].length(); i++) {  
+    for (int i = 0; i < linhas[2].length(); i++)
+    {
       myarray[i] = linhas[2][i] - '0';
     }
-    
+
     int count_two_dig = 0;
-    
+
     for (int i = 0; i < linhas[2].length(); i++)
     {
       cout << "iteração n: " << i << endl;
 
       cout << myarray[i] << " | " << myarray[i + 1] << endl;
 
-
-      if( myarray[i+1] >= 0 && myarray[i+1] <= 9 && myarray[i] > 0){
-        cout << "Numero Completo " << myarray[i] << myarray[i+1] << endl;
-        myarray_clean[i + count_two_dig] = myarray[i]*10+myarray[i+1];
+      if (myarray[i + 1] >= 0 && myarray[i + 1] <= 9 && myarray[i] > 0)
+      {
+        cout << "Numero Completo " << myarray[i] << myarray[i + 1] << endl;
+        myarray_clean[i + count_two_dig] = myarray[i] * 10 + myarray[i + 1];
         count_two_dig += -2;
       }
-      else if(myarray[i+1] < 0 && myarray[i] > 0){
-          cout << "Numero unico: " <<  myarray[i] << '\n';
-          myarray_clean[i + count_two_dig] = myarray[i];
-          count_two_dig += -1;
+      else if (myarray[i + 1] < 0 && myarray[i] > 0)
+      {
+        cout << "Numero unico: " << myarray[i] << '\n';
+        myarray_clean[i + count_two_dig] = myarray[i];
+        count_two_dig += -1;
       }
-      else{
-        cout << "" <<  myarray[i] << '\n';
+      else
+      {
+        cout << "" << myarray[i] << '\n';
       }
 
-      cout << endl ;
+      cout << endl;
     }
-    cout << "end iteration" << endl << endl;
-    
+    cout << "end iteration" << endl
+         << endl;
+
     cout << endl;
 
     for (int i = 0; i < linhas[2].length(); i++)
     {
-      cout << "counter: " << i << " number: " << myarray[i] << " new "<< myarray_clean[i] << '\n';
+      cout << "counter: " << i << " number: " << myarray[i] << " new " << myarray_clean[i] << '\n';
     }
 
     cout << "\n\n############################\n\nLast For:\n";
@@ -110,91 +118,92 @@ File_content *read_instances(const char* relative_file_path) {
     //TODO: Fix the constant here
     for (int i = 0; i < number_of_symbols; i++)
     {
-      cout << "counter: " << i << " number: " << myarray[i] << " new "<< myarray_clean[i] << '\n';
+      cout << "counter: " << i << " number: " << myarray[i] << " new " << myarray_clean[i] << '\n';
     }
 
     cout << "\n\n############################\n\nOthers Values:\n";
     cout << "Number of symbols: " << number_of_symbols << endl;
     cout << "Number m: " << m << endl;
 
-    
     content->number_of_symbols = number_of_symbols;
     content->m = m;
     content->priorities = myarray_clean;
-    
 
     myfile.close();
   }
 
-  else{
+  else
+  {
     cout << "Unable to open file";
-  } 
+    exit(1);
+  }
 
   return content;
 }
 
-int write_res(char verbose, Write_content content_to_write) {
+int write_res(char verbose, Write_content content_to_write)
+{
 
-    int objective          = content_to_write.objective;
-    int number_of_symbols  = content_to_write.number_of_symbols ;
-    int occupied_positions = content_to_write.occupied_positions ;
-    int total_positions    = content_to_write.total_positions;
-    int* avaliable_copies  = content_to_write.avaliable_copies;
-    int* used_copies       = content_to_write.used_copies;
-    int* priorities        = content_to_write.priorities;
-    int* Di                = content_to_write.Di;
-    int* Df                = content_to_write.Df;
-    int* Pi                = content_to_write.Pi;
-    int* Pf                = content_to_write.Pf;
+  int objective = content_to_write.objective;
+  int number_of_symbols = content_to_write.number_of_symbols;
+  int occupied_positions = content_to_write.occupied_positions;
+  int total_positions = content_to_write.total_positions;
+  int *avaliable_copies = content_to_write.avaliable_copies;
+  int *used_copies = content_to_write.used_copies;
+  int *priorities = content_to_write.priorities;
+  int *Di = content_to_write.Di;
+  int *Df = content_to_write.Df;
+  int *Pi = content_to_write.Pi;
+  int *Pf = content_to_write.Pf;
 
+  if (verbose == 'v')
+  {
+    std::cout << "Lista de variáveis:" << endl;
+    std::cout << " objective          " << objective << endl;
+    std::cout << " number_of_symbols  " << number_of_symbols << endl;
+    std::cout << " occupied_positions " << occupied_positions << endl;
+    std::cout << " total_positions    " << total_positions << endl;
+    std::cout << " ARRAY: avaliable_copies  " << avaliable_copies << endl;
+    std::cout << " ARRAY: used_copies       " << used_copies << endl;
+    std::cout << " ARRAY: priorities        " << priorities << endl;
+    std::cout << " ARRAY: Di                " << Di << endl;
+    std::cout << " ARRAY: Df                " << Df << endl;
+    std::cout << " ARRAY: Pi                " << Pi << endl;
+    std::cout << " ARRAY: Pf                " << Pf << endl;
+  }
 
-    if(verbose == 'v'){
-        std::cout << "Lista de variáveis:" << std::endl; 
-        std::cout << " objective          " << objective << endl;
-        std::cout << " number_of_symbols  " << number_of_symbols << endl;
-        std::cout << " occupied_positions " << occupied_positions << endl;
-        std::cout << " total_positions    " << total_positions << endl;
-        std::cout << " ARRAY: avaliable_copies  " << avaliable_copies << endl;
-        std::cout << " ARRAY: used_copies       " << used_copies << endl;
-        std::cout << " ARRAY: priorities        " << priorities << endl;
-        std::cout << " ARRAY: Di                " << Di << endl;
-        std::cout << " ARRAY: Df                " << Df << endl;
-        std::cout << " ARRAY: Pi                " << Pi << endl;
-        std::cout << " ARRAY: Pf                " << Pf << endl;
+  ofstream myfile;
+  myfile.open("./data/res_lnn.txt");
 
-    }
-    
-    ofstream myfile;
-    myfile.open ("./data/res_lnn.txt");
+  for (int i = 1; i < number_of_symbols; i++)
+  {
+    myfile << "Number" << i << "\n";
+  }
+  myfile << "\n**********************************************\n";
+  myfile << "Objective: " << objective << "\n\n";
+  myfile << "Sequence:\n";
+  for (int i = 1; i < number_of_symbols; i++)
+  {
+    myfile << i << " - ";
+  }
+  myfile << "\n\n";
 
-    for (int i = 1; i < number_of_symbols; i++)
-    {
-        myfile << "Number" << i << "\n";
-    }
-    myfile << "\n**********************************************\n";
-    myfile << "Objective: " << objective  << "\n\n";
-    myfile << "Sequence:\n";
-    for (int i = 1; i < number_of_symbols; i++)
-    {
-        myfile << i <<" - ";
-    }
-    myfile << "\n\n";
+  myfile << "Occupied positions: " << occupied_positions << " of " << total_positions << "\n\n";
 
-    myfile << "Occupied positions: " << occupied_positions << " of " << total_positions << "\n\n";
+  for (int i = 0; i < number_of_symbols; i++)
+  {
+    myfile << "symbol " << i + 1 << "--> Priority:  " << priorities[i]
+           << ", Di:  " << Di[i] << ", Df:  " << Df[i] << ", Pi: " << Pi[i] << ", Pf: " << Pf[i] << "\n";
+  }
 
-    for (int i = 0; i < number_of_symbols; i++)
-    {
-        myfile << "symbol " << i+1 << "--> Priority:  "<< priorities[i] <<", Di:  5, Df:  4, Pi: 50, Pf: 40" << "\n";
-    }
+  myfile << "\n(Available copies, Used copies)\n";
 
-    myfile << "\n(Available copies, Used copies)\n";
-    
-    for (int i = 0; i < number_of_symbols; i++)
-    {
-        myfile << "symbol " << i+1 << " -->  " << avaliable_copies[i] << ",  " << used_copies[i] << "\n";  
-    }
-    
-    myfile << "\n**********************************************\n";
-    myfile.close();
-    return 0;
+  for (int i = 0; i < number_of_symbols; i++)
+  {
+    myfile << "symbol " << i + 1 << " -->  " << avaliable_copies[i] << ",  " << used_copies[i] << "\n";
+  }
+
+  myfile << "\n**********************************************\n";
+  myfile.close();
+  return 0;
 }
